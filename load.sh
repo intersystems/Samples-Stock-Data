b22180638b8b6b688eb2e1473f9d8f0914331cb7
dfoster67 <doug.foster@intersystems.com>
2018-11-14 17:27:22 -0500

    Setting executable flag
    Did a git update-index --chmod=+x load.sh
    Should make it so that the correct permission is set when pulled.

--- load.sh
+++ load.sh
@@ -15,6 +15,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+#
 # load Stock Trading Data into iris container
 echo "Installing Stock Trading data..."
 # get directory that we are running in to use as base directory
